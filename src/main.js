b43d2c1ebca7e7189e34aeb4c7b6a2126a19fdd7
Cindy Zhang <cynthiacheung831@gmail.com>
2020-12-09 19:08:59 -0600

    heroku config

--- src/main.js
+++ src/main.js
@@ -7,14 +7,9 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
 import { sync } from 'vuex-router-sync'
 import store from '@/store/store'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
-// Vue.use(BootstrapVue)
-// Vue.use(IconsPlugin)
 
 sync(store, router)
 
